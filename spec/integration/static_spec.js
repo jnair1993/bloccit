32db74198e1b2f664bd36d2376bd061ccb8cec2c
Jayesh Nair <Jayesh1022@gmail.com>
2018-09-13 12:08:15 -0700

    Assignment 1 Completed: Added TDD Marco/Polo

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -13,4 +14,14 @@ describe("routes : static", () => {
     });
 
   });
+
+  describe("GET /marco", () => {
+    it("should return status code 200", (done) => {
+      request.get(base + "marco", (err, res, body) => { //either use variable marco or add "marco" to base
+        expect(res.statusCode).toBe(200);
+        expect(body).toBe("polo");
+        done();
+      })
+    })
+  })
 });
