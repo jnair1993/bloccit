24a48343dc97ffba63cf003dd357e5d435db3f7a
Jayesh Nair <Jayesh1022@gmail.com>
2018-09-14 17:27:19 -0700

    Assignment 3 Completed: Finished a TDD GET request to /about and create a About us .ejs file and connecting route and handler

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,16 +1,28 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const about = "http://localhost:3000/about"
 
 describe("routes : static", () => {
 
   describe("GET /", () => {
-    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
+        done();
       });
     });
 
+  });
+
+  describe("GET /about", () => {
+    it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
+      request.get(about, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+        done();
+      });
+    });
   });
 });
