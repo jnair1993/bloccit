3617b082d1a0c7e0bd3790cce81df2bb94b2e154
Jayesh Nair <Jayesh1022@gmail.com>
2018-09-22 13:53:09 -0700

    Checkpoint 7 Completed: Added post CRUD methods

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,4 +1,5 @@
 const Topic = require("./models").Topic;
+const Post = require("./models").Post;
 
 module.exports = {
   getAllTopics(callback) {
@@ -16,7 +17,13 @@ module.exports = {
     })
   },
   getTopic(id, callback) {
-    return Topic.findById(id).then((topic) => {
+    return Topic.findById(id, {
+      include: [{
+        model: Post,
+        as: "posts"
+      }]
+    })
+    .then((topic) => {
       callback(null, topic);
     }).catch((err) => {
       callback(err);
