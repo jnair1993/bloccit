aad26050ff243912e653eaa524eccba6d7699561
Jayesh Nair <Jayesh1022@gmail.com>
2018-10-01 19:17:35 -0400

    Assignment 7 Post Resource Part 2 Completed: Added Flairs test, route, controllers, and views.

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,10 +4,12 @@ module.exports = {
     const topicRoutes = require("../routes/topics");
     const postRoutes = require("../routes/posts");
     const advertisementRoutes = require("../routes/advertisements");
+    const flairRoutes = require("../routes/flairs");
 
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(postRoutes);
     app.use(advertisementRoutes);
+    app.use(flairRoutes);
   }
 }
