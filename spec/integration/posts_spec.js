60c8b072de36a808f3ee098e0efa8514fd7b582b
Jayesh Nair <Jayesh1022@gmail.com>
2018-10-03 16:16:24 -0400

    Checkpoint 8 Completed: Validation added

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -71,6 +71,31 @@ describe("routes : posts", () => {
       });
     });
 
+    it("should not create a new post that fails validations", (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+          title: "a",
+          body: "b"
+        }
+      };
+
+      request.post(options, (err, res, body) => {
+
+        Post.findOne({
+          where: {
+            title: "a"
+          }
+        }).then((post) => {
+          expect(post).toBeNull();
+          done();
+        }).catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+    });
+
   });
 
   describe("GET /topics/:topicId/posts/:id", () => {
@@ -138,7 +163,8 @@ describe("routes : posts", () => {
       const options = {
         url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
         form: {
-          title: "Snowman Building Competition"
+          title: "Snowman Building Competition",
+          body: "Build the biggest Snowman!"
         }
       };
       request.post(options, (err, res, body) => {
