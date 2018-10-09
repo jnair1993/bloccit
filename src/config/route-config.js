221650bff3847faf6cc7d550cb2eddb2cdfc2d75
Jayesh Nair <Jayesh1022@gmail.com>
2018-10-09 17:18:02 -0400

    Checkpoint 9 Authentication Completed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,11 +3,13 @@ module.exports = {
     const staticRoutes = require("../routes/static");
     const topicRoutes = require("../routes/topics");
     const postRoutes = require("../routes/posts");
+    const userRoutes = require("../routes/users");
     const advertisementRoutes = require("../routes/advertisements");
 
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(postRoutes);
+    app.use(userRoutes);
     app.use(advertisementRoutes);
   }
 }
