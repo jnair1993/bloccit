43f7a4957c66b3be5ebe9a24b29088de53be57e8
Jayesh Nair <Jayesh1022@gmail.com>
2018-10-18 13:30:03 -0400

    Checkpoint 12 Completed: Added voting

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,6 +5,7 @@ module.exports = {
     const postRoutes = require("../routes/posts");
     const userRoutes = require("../routes/users");
     const commentRoutes = require("../routes/comments");
+    const voteRoutes = require("../routes/votes");
     const advertisementRoutes = require("../routes/advertisements");
 
     if(process.env.NODE_ENV === "test") {
@@ -17,6 +18,7 @@ module.exports = {
     app.use(postRoutes);
     app.use(userRoutes);
     app.use(commentRoutes);
+    app.use(voteRoutes);
     app.use(advertisementRoutes);
   }
 }
--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -3,6 +3,7 @@ const Topic = require("./models").Topic;
 const Authorizer = require("../policies/post.js");
 const Comment = require("./models").Comment;
 const User = require("./models").User;
+const Vote = require("./models").Vote;
 
 module.exports = {
   addPost(newPost, callback) {
@@ -14,21 +15,26 @@ module.exports = {
   },
 
   getPost(id, callback) {
-      return Post.findById(id, {
-          include: [{
-              model: Comment,
-              as: "comments",
-              include: [{
-                  model: User
-              }]
-          }]
-      })
-          .then((post) => {
-              callback(null, post);
-          })
-          .catch((err) => {
-              callback(err);
-          })
+    return Post.findById(id, {
+      include: [
+        {
+          model: Comment,
+          as: "comments",
+          include: [
+            {
+              model: User
+            }
+          ]
+        }, {
+          model: Vote,
+          as: "votes"
+        }
+      ]
+    }).then((post) => {
+      callback(null, post);
+    }).catch((err) => {
+      callback(err);
+    })
   },
 
   deletePost(req, callback) {
